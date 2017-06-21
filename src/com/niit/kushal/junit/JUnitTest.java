d3f58bb1459c8e7abba24e6c5664af975a19419e
Kushal Majithia <kushal9892@gmail.com>
2017-06-22 01:20:37 +0530

    jUnit basics!!

---
+++ src/com/niit/kushal/junit/JUnitTest.java
@@ -0,0 +1,22 @@
+package com.niit.kushal.junit;
+
+public class JUnitTest {
+
+}
+
+
+
+//@Test - Specifies that the method is a test method
+//@Before - Runs the method before each test 
+//@After - Runs the method after each test
+//@BeforeClass - Runs once for every test class
+//@AfterClass - Runs once for every test class
+
+//assertEquals method - Takes 2 values (Expected, Actual)
+//assertTrue(true) - success
+//assertTrue(false) - failure
+//assertArrayEquals(Expected, Actual) - 
+//Whenever the code is expected to throw an exception, add the exception class with the @Test annotation
+//@Test(expected=NullPointerException)
+
+/*jUnit is a Regression testing framework*/
